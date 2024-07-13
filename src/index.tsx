eb1f1c58a678ac48352ff6eca6b4e81f42ce2354
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2024-07-13 17:48:28 +0200

    Init setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div>Hello, React!</div>;
+};
+
+const rootElement = document.getElementById("root") as Element;
+const root = createRoot(rootElement);
+
+root.render(<App />);
