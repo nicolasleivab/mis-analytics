861d8d8f03546701b58985bfa89c617d5131e9c6
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2024-07-27 18:54:42 +0200

    Dynamically generating body parts from config file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import AppRouter from "./router/router";
 import "./styles/reset.css";
+import "./styles/body-parts.css";
 import "./styles/variables.css";
 
 const rootElement = document.getElementById("root") as Element;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { BodyViz } from "../../components";
 import { Card, Flex } from "../../layout";
 import * as styles from "./Dashboard.module.css";
+import { BODY_PARTS } from "../../components/BodyViz/body-parts";
 
 export default function Dashboard() {
+  const [selected, setSelected] = useState(BODY_PARTS[0].name);
+
   return (
     <div className={styles.Dashboard}>
       <Flex direction="column">
         <h2>Dashboard</h2>
         <Flex justifyContent="flex-start">
           <Card>
-            <BodyViz />
+            <BodyViz
+              selected={selected}
+              onPartClick={(part) => setSelected(part)}
+            />
           </Card>
           <div></div>
         </Flex>
