ef16af5fe26251924eb49b7b5d2246b367137d27
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2024-12-15 17:42:45 +0100

    Refactoring app router creation

--- application/App.tsx
+++ application/App.tsx
@@ -1,10 +1,29 @@
-import AppRouter from './controller/Router/AppRouter';
-import { createTheme } from '@mantine/core';
+import { createAppRouter } from './controller/Router/AppRouter';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { ChakraProvider } from '@chakra-ui/react';
+import {
+  TabsProvider,
+  ThemeProvider,
+  ExcelProvider,
+  ThemeWrapper,
+} from '../model/context';
 
 const theme = createTheme({
-  /** Mantine theme override */
+  // Mantine theme overrides
 });
 
 export default function App() {
-  return <AppRouter theme={theme} />;
+  return (
+    <ThemeProvider>
+      <MantineProvider theme={theme}>
+        <ChakraProvider>
+          <ThemeWrapper>
+            <TabsProvider>
+              <ExcelProvider>{createAppRouter()}</ExcelProvider>
+            </TabsProvider>
+          </ThemeWrapper>
+        </ChakraProvider>
+      </MantineProvider>
+    </ThemeProvider>
+  );
 }
--- application/controller/Router/AppRouter.tsx
+++ application/controller/Router/AppRouter.tsx
@@ -1,70 +1,36 @@
-import {
-  Route,
-  Routes,
-  BrowserRouter as Router,
-  // Navigate,
-} from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import { APP_ROUTES } from './routes';
 import Nav from '../Nav/Nav';
-import { MantineProvider, MantineThemeOverride } from '@mantine/core';
-import { ChakraProvider } from '@chakra-ui/react';
-import {
-  TabsProvider,
-  ThemeProvider,
-  ExcelProvider,
-  ThemeWrapper,
-} from '../../../model/context';
+import React from 'react';
 
-export default function AppRouter({ theme }: { theme: MantineThemeOverride }) {
-  return (
-    <ThemeProvider>
-      <MantineProvider theme={theme}>
-        <ChakraProvider>
-          <ThemeWrapper>
-            <TabsProvider>
-              <ExcelProvider>
-                <Router>
-                  <div>
-                    <Nav />
-                    <Routes>
-                      {APP_ROUTES.map((route) => (
-                        <Route
-                          key={route.id}
-                          path={route.path}
-                          element={
-                            <PrivateRoute>
-                              <route.component />
-                            </PrivateRoute>
-                          }
-                        />
-                      ))}
-                    </Routes>
-                  </div>
-                </Router>
-              </ExcelProvider>
-            </TabsProvider>
-          </ThemeWrapper>
-        </ChakraProvider>
-      </MantineProvider>
-    </ThemeProvider>
-  );
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  // TODO: Implement authentication for private routes
+  // If the user is not authorized, you could redirect here.
+  return <>{children}</>;
 }
 
-function PrivateRoute({
-  children,
-  // id,
-}: {
-  children: React.ReactNode;
-  // id: string;
-}) {
-  // TODO: Implement authentication for private routes
-  // const { views } = useSelector(selectUser);
-
-  // if (id === APP_ROUTES[0].id || views?.includes(id)) {
-  //   return children;
-  // }
-
-  // return <Navigate to="/" />;
-
-  return children;
+// Now this is just a function that returns the rendered JSX.
+// Note: It's no longer a React component by strict definition;
+// it's a helper function returning React elements.
+export function createAppRouter() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Routes>
+          {APP_ROUTES.map((route) => (
+            <Route
+              key={route.id}
+              path={route.path}
+              element={
+                <PrivateRoute>
+                  <route.component />
+                </PrivateRoute>
+              }
+            />
+          ))}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
