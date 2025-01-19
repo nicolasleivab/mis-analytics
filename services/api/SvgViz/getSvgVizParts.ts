4f4e666590e33554ea12cab5dfbc4ede7e4cafb1
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2025-01-19 18:02:06 +0100

    Adding brain demo viz

--- services/api/SvgViz/getSvgVizParts.ts
+++ services/api/SvgViz/getSvgVizParts.ts
@@ -1,5 +1,6 @@
 import { TSvgPart, TClipPath } from '../../../model';
 import demoViz from './demoViz.json'; // DEMO DATA
+import brainViz from './brainViz.json'; // BRAIN VIZ
 import demoClipPaths from './demoClipPaths.json'; // DEMO CLIP PATHS
 
 export type TSvgPartsData = {
@@ -15,8 +16,8 @@ export async function getSvgVizParts(): Promise<{
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        svgParts: demoViz,
-        clipPaths: demoClipPaths,
+        svgParts: brainViz,
+        clipPaths: [], // demoClipPaths,
       });
     }, 1000); // 1 second delay to simulate async behavior
   });
