08b05fc2f6adf288c76bcf10b9ae296f1097777e
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2025-03-02 17:27:31 +0100

    Updating color palettes

--- presentation/styles/colors.ts
+++ presentation/styles/colors.ts
@@ -1,18 +1,13 @@
 export const darkColorPalette1 = [
-  '#33b1ff',
-  '#ba4e00',
-  '#6fdc8c',
-  '#ff7eb6',
-  '#005d5d',
-  '#d2a106',
-  '#007d79',
-  '#8a3ffc',
-  '#bae6ff',
-  '#d12771',
-  '#08bdba',
-  '#4589ff',
-  '#d4bbff',
-  '#fa4d56',
+  '#fd7f6f',
+  '#7eb0d5',
+  '#b2e061',
+  '#bd7ebe',
+  '#ffb55a',
+  '#ffee65',
+  '#beb9db',
+  '#fdcce5',
+  '#8bd3c7',
 ];
 
 export const lightColorPalette1 = [
